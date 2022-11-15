733fc91c23aa247bf1c4cfc63484e24ecd17a35c
Najman <najman.husaini@utah.edu>
2022-11-14 22:21:15 -0700

    added links

--- website/src/components/Programming.js
+++ website/src/components/Programming.js
@@ -11,26 +11,26 @@ class Programming extends React.Component {
                 you do not need to know any of these if you are a strong, independent learner!<br />
                 <br />
                 Linux (working in the command line)<br />
-                ROS 2 (or ROS 1. If you know this you are already very valuable!)<br />
-                Python<br />
-                GitHub<br />
+                <a href="https://docs.ros.org/en/foxy/index.html">ROS 2</a> (or ROS 1. If you know this you are already very valuable!)<br />
+                <a href="https://www.python.org/">Python</a><br />
+                <a href="https://github.com/">GitHub</a><br />
                 C++ (very optional, but is occasionally useful)<br />
-                Docker<br />
+                <a href="https://www.docker.com/">Docker</a><br />
                 <br />
                 We use these technologies when we are developing our robot. Besides that, we also want to branch out in what<br />
                 we do, starting with redeveloping this very website! If you have some experience in web-development, or have<br />
                 any interest, please consider joining! We intend to use these languages:<br />
                 <br />
-                JavaScript/TypeScript (specifically for Svelte)<br />
-                Rust (optional, but also very cool!)<br />
+                JavaScript/TypeScript (specifically for <a href="https://svelte.dev/">Svelte</a>)<br />
+                <a href="https://www.rust-lang.org/">Rust</a> (optional, but also very cool! We want to use <a href="https://rocket.rs/">Rocket</a>)<br />
                 <br />
                 Lastly, machine learning is an interest of ours, but it often takes up too much time given the team sizes we've<br />
                 had in the past. If you have experience in this and are interested in contributing, we would love to onboard you!<br />
                 If we were to implement machine learning, it would ideally be through Python. If you are interested in learning<br />
                 machine learning for the club, refer to the following technologies:<br />
                 <br />
-                Keras (lean more towards audio interpretation)<br />
-                Colaboratory/Jupyter<br />
+                <a href="https://keras.io/">Keras</a> (lean more towards audio interpretation)<br />
+                <a href="https://colab.research.google.com/">Colaboratory</a>/<a href="https://jupyter.org/">Jupyter</a><br />
                 <br />
                 Want to join? Send an email to our current programming team lead here: <a href="mailto:name@email.com">najman.husaini@utah.edu</a><br />
                 We meet every week on Friday at 4pm in MEK 2445. Feel free to pass by if you want to see what we do before you apply!<br />
